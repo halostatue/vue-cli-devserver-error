4e9e80575f652d290576b3e966034c107b22e2ea
Austin Ziegler <austin@zieglers.ca>
2022-03-27 01:27:39 -0400

    Updating test file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
+const path = require('path')
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: 'all',
+    static: [
+      {
+        publicPath: `/test/`,
+        directory: path.join(__dirname, 'public'),
+      },
+      {
+        publicPath: `/test/`,
+        directory: path.join(__dirname, 'public2'),
+      },
+    ],
+  },
 })
